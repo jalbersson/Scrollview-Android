5e7c243d12a34971955788f936344dd4a57a3626
Jalbersson Guillermo Plazas <Jalbersson Guillermo Plazas>
2017-03-31 09:00:34 -0500

    Initial commit

---
+++ app/src/main/java/com/example/jalberssonplazas/scrollview/horizontalActivity.java
@@ -0,0 +1,13 @@
+package com.example.jalberssonplazas.scrollview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class horizontalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_horizontal);
+    }
+}
---
+++ app/src/main/java/com/example/jalberssonplazas/scrollview/inicioActivity.java
@@ -0,0 +1,15 @@
+package com.example.jalberssonplazas.scrollview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class inicioActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio);
+    }
+}
